aeeae51dbeb9099443617eb3d31f0d44fc81c419
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-02-14 20:26:52 +0500

    added stylelint

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -20,7 +20,7 @@ export const Sidebar = ({ className }:SidebarProps) => {
                 className,
             ])}
         >
-            <button type="button" onClick={onToggle}>toogle</button>
+            <button type="button" onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
