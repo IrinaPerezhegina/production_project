27820bfee0b28bf16dec7b0884e1e752cbc9faef
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-04-13 21:58:29 +0500

    React Testing Library changed

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,14 @@
-import { render ,screen} from "@testing-library/react";
-import { Button, ThemeButton } from "shared/ui/Button/Button";
+import { render, screen } from '@testing-library/react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
-    test('button', () => {
-        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
-        expect(screen.getByText('TEST')).toHaveClass("clear")
+    test('Test render', () => {
+        render(<Button>TEST</Button>);
+        expect(screen.getByText('TEST')).toBeInTheDocument();
     });
-    
+    test('Test render theme', () => {
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        expect(screen.getByText('TEST')).toHaveClass('clear');
+        screen.debug();
+    });
 });
