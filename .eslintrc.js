5fc06b6c0ee02416d9f2bf7f076e56ddfbfe3e84
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-04-14 09:46:32 +0500

    error correction

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,11 +42,22 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         '@typescript-eslint/no-unused-vars': ['warn'],
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true,
+            ignoreAttribute: ['data-testid'],
+        }],
         'linebreak-style': 'off',
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,5 +1,7 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import {
+    renderWithTranslation,
+} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
@@ -9,9 +11,9 @@ describe('Sidebar', () => {
     });
     test('test toggle', () => {
         renderWithTranslation(<Sidebar />);
-        const toggleBtn=screen.getByTestId("sidebar-toggle")
-         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
-        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
     });
 });
