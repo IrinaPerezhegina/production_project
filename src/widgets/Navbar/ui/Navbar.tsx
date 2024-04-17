028d11d4dece04699a0b087ac4fa242c8188f51c
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-04-17 17:08:13 +0500

    change Sidebar, screenshot tests

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable i18next/no-literal-string */
 import { classNames } from 'shared/lib/ClassNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps{
@@ -9,24 +7,11 @@ interface NavbarProps{
 
 }
 
-export const Navbar = ({ className }:NavbarProps) => {
-    const { t } = useTranslation('translation');
-    return (
-        <div className={classNames(cls.Navbar, {}, [className])}>
-
-            <div className={cls.links}>
-                <AppLink
-                    to="/"
-                    theme={AppLinkTheme.SECONDARY}
-                    className={cls.mainLink}
-                >
-                    {t('main')}
-                </AppLink>
-                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                    {t('about')}
-                </AppLink>
-            </div>
+export const Navbar = ({ className }:NavbarProps) => (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+        <div className={cls.links}>
+            /
         </div>
-    );
-};
+    </div>
+);
 export default Navbar;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routerConfig/routerConfig';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import MainIcon from 'shared/assets/icons/main.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -14,7 +19,7 @@ export const Sidebar = ({ className }:SidebarProps) => {
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
-
+    const { t } = useTranslation('translation');
     return (
         <div
             data-testid="sidebar"
@@ -32,8 +37,27 @@ export const Sidebar = ({ className }:SidebarProps) => {
                 square
             >
                 {collapsed ? '>' : '<'}
-
             </Button>
+            <div className={cls.items}>
+                <AppLink
+                    to={RoutePath.main}
+                    theme={AppLinkTheme.SECONDARY}
+                    className={cls.item}
+                >
+                    <MainIcon className={cls.icon} />
+                    <span className={cls.link}>{t('main')}</span>
+                </AppLink>
+
+                <AppLink
+                    to={RoutePath.about}
+                    theme={AppLinkTheme.SECONDARY}
+                    className={cls.item}
+                >
+                    <AboutIcon className={cls.icon} />
+                    <span className={cls.link}>{t('about')}</span>
+                </AppLink>
+
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher
