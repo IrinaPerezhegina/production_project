9a5a462f356beace3616f795e2dc216fcb123bc2
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-04-20 20:27:09 +0500

    fix lint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,20 @@
 import './styles/index.scss';
 import { classNames } from 'shared/lib/ClassNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { AppRouter } from 'app/providers/router';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 
-const App = () => {
-    const { theme } = useTheme();
-
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
-        </div>
-    );
-};
+const App = () => (
+    <div className={classNames('app', {}, [])}>
+        <Suspense fallback="">
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </Suspense>
+    </div>
+);
 
 export default App;
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,7 +3,6 @@ import React, {
     ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
-import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
@@ -23,7 +22,6 @@ export const Modal = (props:ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef < ReturnType<typeof setTimeout>>();
-    const { theme } = useTheme();
     const closeHandler = useCallback(() => {
         if (onClose) {
             setIsClosing(true);
