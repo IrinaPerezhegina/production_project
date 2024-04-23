2a9827e5b7b1b8054e8654280244642aa33c0623
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-04-23 17:17:56 +0500

    add loginSlice

---
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -0,0 +1,12 @@
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// const loginByUsername = createAsyncThunk(
+//     'login/loginByUsername',
+//     async ({ username, password }) => {
+//         const response = await axios.post('https://localhost:8000/login', {
+
+//         });
+//         return response.data;
+//     },
+// );
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,32 +1,56 @@
 import { classNames } from 'shared/lib/ClassNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { getLoginState } from 'features/AuthByUsername/model/selectors/getLoginState/getLoginState';
+import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
 className?: string;
 }
 
-export const LoginForm = ({ className }:LoginFormProps) => {
+export const LoginForm = memo(({ className }:LoginFormProps) => {
     const { t } = useTranslation('translation');
+    const dispatch = useDispatch();
+    const { username, password } = useSelector(getLoginState);
+    const onChangeUsername = useCallback((value:string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value:string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
+    const onLoginClick = useCallback(() => {
+
+    }, []);
+
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
             <Input
                 autofocus
                 placeholder={t('enter username')}
                 className={cls.input}
+                onChange={onChangeUsername}
+                value={username}
             />
             <Input
                 placeholder={t('enter password')}
                 className={cls.input}
+                onChange={onChangePassword}
+                value={password}
             />
             <Button
+                theme={ButtonTheme.OUTLINE}
                 className={cls.loginBtn}
+                onClick={onLoginClick}
             >
                 {t('enter')}
             </Button>
         </div>
 
     );
-};
+});
