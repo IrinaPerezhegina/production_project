f588ee4e586f8a36f91b1ced0a44167226b94fad
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-05-03 10:49:30 +0500

    add instance API, ApiUrl

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -32,7 +32,8 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve?.extensions?.push('.ts', 'tsx');
     config.module?.rules?.push(buildCssLoader(true));
     config?.plugins?.push(new DefinePlugin({
-        __IS_DEV__: true,
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify(''),
     }));
 
     return config;
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -17,3 +17,4 @@ export default svg;
 }
 
 declare const __IS_DEV__:boolean;
+declare const __API__:string;
