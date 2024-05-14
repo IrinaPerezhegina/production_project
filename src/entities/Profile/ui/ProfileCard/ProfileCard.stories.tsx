e2caa8eb691f0f2a2ad47b72298c638188f14386
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-05-14 12:02:20 +0500

    add __PROJECT__

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import avatar from 'shared/assets/tests/avatar.jpg';
+import { ProfileCard } from './ProfileCard';
+
+export default {
+    title: 'entities/ProfileCard',
+    component: ProfileCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    data: {
+        first: 'Irina',
+        username: 'admin',
+        age: 34,
+        country: Country.Russia,
+        lastname: 'Perezhegina',
+        currency: Currency.RUB,
+        city: 'Tyumen',
+        avatar,
+    },
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+    error: 'true',
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+    isLoading: true,
+};
