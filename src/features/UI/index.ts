122b2070ef834b1874de0051f25335320276d9e4
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-06-09 12:57:14 +0500

    add useThrottle, scroll

---
+++ src/features/UI/index.ts
@@ -0,0 +1,5 @@
+export { UIReducer, UIActions } from './model/slices/UISlice';
+
+export { getUIScrollByPath } from './model/selectors/ui';
+
+export { UISchema } from './model/types/UISchema';
---
+++ src/widgets/Page/Page.tsx
@@ -0,0 +1,68 @@
+import { classNames } from 'shared/lib/ClassNames/classNames';
+import {
+    MutableRefObject,
+    ReactNode,
+    useRef,
+    UIEvent,
+} from 'react';
+import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { UIActions } from 'features/UI';
+import { useLocation } from 'react-router-dom';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { getUIScrollByPath } from 'features/UI/model/selectors/ui';
+import { useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
+import cls from './Page.module.scss';
+
+interface PageProps {
+   className?: string;
+   children:ReactNode;
+   onScrollEnd?:()=>void
+}
+
+export const Page = (props:PageProps) => {
+    const {
+        className,
+        children,
+        onScrollEnd,
+    } = props;
+    const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const dispatch = useAppDispatch();
+    const { pathname } = useLocation();
+    const scrollPosition = useSelector((state:StateSchema) => getUIScrollByPath(state, pathname));
+
+    useInitialEffect(() => {
+        wrapperRef.current.scrollTop = scrollPosition;
+    });
+
+    const onScroll = useThrottle((e:UIEvent<HTMLDivElement>) => {
+        dispatch(UIActions.setScrollPosition({
+            path: pathname,
+            position: e.currentTarget.scrollTop,
+        }));
+    }, 500);
+
+    useInfiniteScroll({
+        triggerRef,
+        wrapperRef,
+        callback: onScrollEnd,
+
+    });
+
+    return (
+        <section
+            ref={wrapperRef}
+            className={classNames(cls.page, {}, [className])}
+            onScroll={onScroll}
+        >
+            {children}
+            <div
+                ref={triggerRef}
+            />
+        </section>
+
+    );
+};
