7671cbfa362f87975292fa9681191fdb51e49f72
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-06-13 16:26:45 +0500

    add ArticleSortSelector

--- src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -1,5 +1,8 @@
 import { classNames } from 'shared/lib/ClassNames/classNames';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
+import { Select, SelectOption } from 'shared/ui/Select/Select';
+import { useTranslation } from 'react-i18next';
+import { ArticleSortField } from 'entities/Article/model/types/article';
 import cls from './ArticleSortSelector.module.scss';
 
 interface ArticleSortSelectorProps {
@@ -8,10 +11,51 @@ interface ArticleSortSelectorProps {
 
 export const ArticleSortSelector = memo((props:ArticleSortSelectorProps) => {
     const { className } = props;
+    const { t } = useTranslation('article');
+    const orderOptions = useMemo<SelectOption[]>(
+        () => [
+            {
+                value: 'asc',
+                content: t('ascending'),
+            },
+            {
+                value: 'desc',
+                content: t('descending'),
+            },
+        ],
+
+        [t],
+    );
+
+    const sortFieldOptions = useMemo<SelectOption[]>(
+        () => [
+            {
+                value: ArticleSortField.CREATED,
+                content: t('date of creation'),
+            },
+            {
+                value: ArticleSortField.TITLE,
+                content: t('name'),
+            },
+            {
+                value: ArticleSortField.VIEWS,
+                content: t('views'),
+            },
+        ],
+
+        [t],
+    );
 
     return (
         <div className={classNames(cls.articleSortSelector, {}, [className])}>
-            {/*  */}
+            <Select
+                options={sortFieldOptions}
+                label={t('sort by')}
+            />
+            <Select
+                options={orderOptions}
+                label={t('by')}
+            />
         </div>
     );
 });
--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/ClassNames/classNames';
 import { memo, useCallback } from 'react';
-import { ArticleViewSelector } from 'entities/Article';
+import { ArticleView, ArticleViewSelector } from 'entities/Article';
 import { useSelector } from 'react-redux';
 import { Select } from 'shared/ui/Select/Select';
 import { Card } from 'shared/ui/Card/Card';
 import { Input } from 'shared/ui/Input/Input';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { articlesPageActions } from 'pages/ArticlesPage/model/slices/articlePageSlice';
 import cls from './ArticlesPageFilters.module.scss';
 import { getArticlesPageView } from '../../model/selectors/articlesPageSelectors';
 
@@ -16,11 +18,11 @@ interface ArticlesPageFiltersProps {
 export const ArticlesPageFilters = memo((props:ArticlesPageFiltersProps) => {
     const { className } = props;
     const { t } = useTranslation('article');
-
+    const dispatch = useAppDispatch();
     const view = useSelector(getArticlesPageView);
-    const onChangeView = useCallback(() => {
-
-    }, []);
+    const onChangeView = useCallback((view:ArticleView) => {
+        dispatch(articlesPageActions.setView(view));
+    }, [dispatch]);
 
     return (
         <div className={classNames(cls.articlesPageFilters, {}, [className])}>
