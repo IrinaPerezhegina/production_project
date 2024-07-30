fc9bf9802c179a43f6a3681b80bc037dea5cd33f
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-07-30 12:26:41 +0500

    add createArticleRecommendation

---
+++ src/features/articleRecommendationsList/api/articleRecommendationsApi.ts
@@ -0,0 +1,28 @@
+import { rtkApi } from 'shared/api/rtkApi';
+
+const recommendationsApi = rtkApi.injectEndpoints({
+    endpoints: (build) => ({
+        getArticleRecommendationsList: build.query({
+            query: (limit) => ({
+                url: '/articles',
+                params: {
+                    _limit: limit,
+                },
+            }),
+        }),
+        // createArticleRecommendation: build.mutation({
+        //     query: (limit) => ({
+        //         url: '/articles',
+        //         params: {
+        //             _limit: limit,
+        //         },
+        //         method: 'POST',
+        //     }),
+        // }),
+    }),
+});
+export const useArticleRecommendationsList = recommendationsApi
+    .useGetArticleRecommendationsListQuery;
+
+// export const useCreateArticleRecommendation = recommendationsApi
+//     .useCreateArticleRecommendationMutation;
