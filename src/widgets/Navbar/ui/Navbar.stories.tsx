91ed4a7936d2602bae085bd1d2c886719a5ee2b4
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-08-13 13:40:06 +0500

    fix mistakes

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -2,7 +2,17 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Notification } from 'entities/Notification/model/types/notification';
+import withMock from 'storybook-addon-mock';
 import { Navbar } from './Navbar';
+
+const notification:Notification = {
+    id: '1',
+    description: 'Title',
+    title: 'Description',
+    userId: '2',
+
+};
 
 export default {
     title: 'widgets/Navbar',
@@ -10,6 +20,7 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof Navbar>;
 
 const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
@@ -25,3 +36,20 @@ Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
 export const AuthNavbar = Template.bind({});
 AuthNavbar.args = {};
 AuthNavbar.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({ user: { authData: {} } })];
+AuthNavbar.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/notifications`,
+            method: 'GET',
+            status: 200,
+            response: [
+                { ...notification, id: '1' },
+                { ...notification, id: '2' },
+                { ...notification, id: '3' },
+                { ...notification, id: '4' },
+                { ...notification, id: '5' },
+                { ...notification, id: '6' },
+            ],
+        },
+    ],
+};
