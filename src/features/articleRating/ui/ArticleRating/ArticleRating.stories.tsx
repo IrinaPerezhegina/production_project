1fc4ba9824e593ece74c53f968e7799340a8ebe4
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-08-29 16:33:11 +0500

    refactoring code

--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -3,6 +3,8 @@ import withMock from 'storybook-addon-mock';
 import ArticleRating from './ArticleRating';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Rating } from '@/entities/Rating';
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '@/app/providers/ThemeProvider';
 
 const rate:Rating = {
     feedback: 'good',
@@ -22,18 +24,42 @@ const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating 
 
 export const Normal = Template.bind({});
 Normal.args = {
-
+    articleId: '1',
 };
-Normal.decorators = [StoreDecorator({})];
+Normal.decorators = [StoreDecorator({
+    user: {
+        authData: { id: '1' },
+    },
+})];
 Normal.parameters = {
     mockData: [
         {
-            url: `${__API__}/article-ratings?userId=1`,
+            url: `${__API__}/article-ratings?userId=1&articleId=1`,
             method: 'GET',
             status: 200,
             response: [
-                { ...rate, rate: 3 },
+                { ...rate, rate: 4 },
             ],
+        },
+    ],
+};
+
+export const WithoutRate = Template.bind({});
+WithoutRate.args = {
+    articleId: '1',
+};
+WithoutRate.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    user: {
+        authData: { id: '1' },
+    },
+})];
+WithoutRate.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/article-ratings?userId=1&articleId=1`,
+            method: 'GET',
+            status: 200,
+            response: [],
         },
     ],
 };
