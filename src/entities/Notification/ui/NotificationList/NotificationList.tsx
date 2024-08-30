921f73809156f45b5877f1812fb270213057737b
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-08-30 10:05:47 +0500

    refactoring code

--- src/entities/Notification/ui/NotificationList/NotificationList.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -11,7 +11,9 @@ interface NotificationListProps {
 }
 
 export const NotificationList = memo((props:NotificationListProps) => {
-    const { className } = props;
+    const {
+        className,
+    } = props;
 
     const { data, isLoading } = useNotifications(null, {
         pollingInterval: 5000,
