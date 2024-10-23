54609b15ca5040f662e771170fc41e4affc7efd4
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-10-23 16:16:09 +0500

    add e2e test for profile

--- cypress/support/commands/login.ts
+++ cypress/support/commands/login.ts
@@ -1,15 +1,23 @@
+import { User } from '../../../src/entities/User/model/types/user';
 import { USER_LOCALSTORAGE_KEY } from '../../../src/shared/const/localstorage';
 
-export const login = (username:string = 'testuser', password:string = '123') => {
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:8000/login',
-        body: {
-            username,
-            password,
-        },
-    }).then(({ body }) => {
-        window.localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(body));
-        cy.visit('/');
-    });
-};
+export const login = (username:string = 'testuser', password:string = '123') => cy.request({
+    method: 'POST',
+    url: 'http://localhost:8000/login',
+    body: {
+        username,
+        password,
+    },
+}).then(({ body }) => {
+    window.localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(body));
+    return body;
+});
+
+declare global {
+    namespace Cypress {
+      interface Chainable {
+        login(email?: string, password?: string): Chainable<User>
+
+      }
+    }
+  }
