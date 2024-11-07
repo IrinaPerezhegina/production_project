d127386a92f1dbfcc20688e69f2c90ff2d5100eb
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-11-07 14:41:18 +0500

    2 the concept of Feature flags. Gradual introduction of new features

---
+++ src/shared/features/setGetFeatures.ts
@@ -0,0 +1,14 @@
+import { FeatureFlags } from '../types/featureFlags';
+
+// Фичи не меняются в рамках сессии, их необязательно делать реактивными
+let featureFlags: FeatureFlags;
+
+export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
+    if (newFeatureFlags) {
+        featureFlags = newFeatureFlags;
+    }
+}
+
+export function getFeatureFlags(flag: keyof FeatureFlags) {
+    return featureFlags[flag];
+}
